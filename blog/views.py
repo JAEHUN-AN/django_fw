b035420f690707530a807446f214cf63e95a93c4
jaehun <dybe15@gmail.com>
2021-01-13 12:14:32 +0900

    django view_url

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+"""from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+    name = 'Django'
+    return HttpResponse('''<h1>Hello Django</h1>
+                        <p>{name}</p>'''.format(name=name))
+"""
+def post_list(request):
+    return render(request,'blog/post_list.html')
